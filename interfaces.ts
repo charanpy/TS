d1dcdaf178954f2788fc321c3d945e14ae1572eb
charan <charan9487@gmail.com>
2020-12-26 13:02:34 +0530

    type and interface definition for functions

--- interfaces.ts
+++ interfaces.ts
@@ -1,3 +1,17 @@
+type AddType = (a:number,b:number)=>number;
+
+let addType:AddType=(n1:number,n2:number)=>n1+n2;
+
+//interface
+interface AddInterface{
+ (a:number,b:number):number;
+}
+let addInterface:AddInterface=(n1:number,n2:number)=>{
+  return n1+n2;
+}
+
+
+
 interface Named{
  name:string;
 }
