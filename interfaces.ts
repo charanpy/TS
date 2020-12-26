e26194e36978c0a6a1ff87a27ee1953c3ae4dd0c
charan <charan9487@gmail.com>
2020-12-26 12:30:24 +0530

    interface

---
+++ interfaces.ts
@@ -0,0 +1,15 @@
+interface Person {
+  name:string;
+  age:number;
+  greet(phrase:string):void;
+}
+
+let user1:Person;
+user1={
+  name:'ch',
+  age:18,
+  greet(phrase:string){
+    console.log(phrase+this.name);
+  }
+}
+user1.greet("Hi");
