1befbd9b84476e6daeee2abd422c87b7260e7cc7
charan <charan9487@gmail.com>
2020-12-28 10:24:29 +0530

    built in generics

---
+++ Generics.ts
@@ -0,0 +1,11 @@
+//Built-in
+const names:any[] =[];
+
+const name1:Array<string> = [];
+
+const promise:Promise<string> = new Promise((resolve,reject)=>{
+  setTimeout(()=>{
+    resolve('This is done')
+  },2000);
+});
+
