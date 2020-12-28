eccda21154bf1fc74a65c29defa2e4ca88e65eb7
charan <charan9487@gmail.com>
2020-12-28 11:55:23 +0530

    generic class

--- Generics.ts
+++ Generics.ts
@@ -40,3 +40,26 @@ console.log(extractAndConvert(
   {name:'cj'},"name"
 ))
 
+class DataStorage<T>{
+  private data :T[]=[]
+
+  addItem(item:T){
+    this.data.push(item);
+  }
+
+  removeItem(item:T){
+    this.data.splice(this.data.indexOf(item),1)
+  }
+
+  getItems(){
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem('Ma');
+
+const noStorage = new DataStorage<number>();
+noStorage.addItem(2);
+
+console.log(textStorage,noStorage);
